af5c992f8879571083e2dafa31a0512809d4a9ec
Sidney San Martín <sidney@keyba.se>
2015-09-18 18:31:29 -0400

    Support ^Z, add some comments

--- miniline.go
+++ miniline.go
@@ -3,6 +3,7 @@ package miniline
 import (
 	"bufio"
 	"os"
+	"syscall"
 
 	"golang.org/x/crypto/ssh/terminal"
 )
@@ -15,18 +16,55 @@ func (_ InterruptedError) Error() string {
 
 var ErrInterrupted error = InterruptedError{}
 
+type tty struct {
+	fd    uintptr
+	state *terminal.State
+}
+
+func (tty *tty) enterRaw() (err error) {
+	tty.state, err = terminal.MakeRaw(int(tty.fd))
+	return
+}
+
+func (tty *tty) exitRaw() (err error) {
+	err = terminal.Restore(int(tty.fd), tty.state)
+	tty.state = nil
+	return
+}
+
 type lineReader struct {
+	prompt string
 	reader *bufio.Reader
 	writer *bufio.Writer
+	tty    tty
 	buf    []byte
 	pos    int
 }
 
+func newLineReader(ttyFile *os.File, prompt string) lineReader {
+	return lineReader{
+		prompt: prompt,
+		reader: bufio.NewReader(ttyFile),
+		writer: bufio.NewWriter(ttyFile),
+		tty:    tty{fd: ttyFile.Fd()},
+	}
+}
+
+// esc prints an escape sequence with the given string and is used to control
+// the terminal. Common ones (e.g. for moving the cursor) are documented here:
+// https://en.wikipedia.org/wiki/ANSI_escape_code#CSI_codes
 func (lr *lineReader) esc(s string) (err error) {
 	_, err = lr.writer.WriteString("\x1b[" + s)
 	return
 }
 
+func (lr *lineReader) pprompt() (err error) {
+	_, err = lr.writer.WriteString(lr.prompt)
+	return
+}
+
+// pbuf re-writes the lr.buf to the terminal starting at lr.pos, and restores
+// the cursor to its previous position. Used when editing the middle of a line.
 func (lr *lineReader) pbuf() (err error) {
 	err = lr.esc("s")
 	if err == nil {
@@ -38,6 +76,7 @@ func (lr *lineReader) pbuf() (err error) {
 	return
 }
 
+// backspace removes the character to the left of the cursor, if any.
 func (lr *lineReader) backspace() (err error) {
 	if len(lr.buf) == 0 {
 		return
@@ -61,6 +100,8 @@ func (lr *lineReader) backspace() (err error) {
 	return
 }
 
+// readEscape is called after a the ESC character has been read from stdin. It
+// expects an escape sequence to follow.
 func (lr *lineReader) readEscape() (err error) {
 	b, err := lr.reader.ReadByte()
 	if err != nil {
@@ -92,7 +133,18 @@ func (lr *lineReader) readEscape() (err error) {
 	return
 }
 
+// readLine is lineReader's entry point. It reads a line of user input.
 func (lr *lineReader) readLine() error {
+
+	lr.tty.enterRaw()
+	defer lr.tty.exitRaw()
+
+	lr.pprompt()
+	defer func() {
+		lr.writer.WriteString("\n")
+		lr.writer.Flush()
+	}()
+
 	for {
 		if err := lr.writer.Flush(); err != nil {
 			return err
@@ -102,22 +154,38 @@ func (lr *lineReader) readLine() error {
 		if err != nil {
 			return err
 		}
+		if b <= 26 { // ctrl + letter
+			switch b {
+			case 3: // ^C
+				return ErrInterrupted
+			case 4, 13: // ^D, ^M (which is also the return key)
+				return nil
+			case 26: // ^Z
+				lr.tty.exitRaw()
+				syscall.Kill(0, syscall.SIGSTOP)
+				lr.tty.enterRaw()
+				lr.pprompt()
+				lr.writer.Write(lr.buf[:lr.pos])
+				lr.pbuf()
+				continue
+			default:
+				continue
+			}
+		}
+
 		switch b {
-		case 0x4, byte('\r'): // EOF or newline
-			return nil
 		case 0x7F: // backspace
 			if err := lr.backspace(); err != nil {
 				return err
 			}
 			continue
-		case 0x03: // ^C
-			return ErrInterrupted
 		case 0x1b: // ESC
 			if err := lr.readEscape(); err != nil {
 				return err
 			}
 			continue
 		}
+
 		lr.writer.WriteByte(b)
 
 		if lr.pos == len(lr.buf) {
@@ -133,16 +201,9 @@ func (lr *lineReader) readLine() error {
 	}
 }
 
-func WithRaw(fd uintptr, fn func() error) (err error) {
-	origState, err := terminal.MakeRaw(int(fd))
-	if err != nil {
-		return
-	}
-	defer terminal.Restore(int(fd), origState)
-	err = fn()
-	return
-}
-
+// ReadLine returns a line of user input (terminated by a newline or or ^D)
+// read from the tty. The given prompt is printed first. If the user types ^C,
+// ReadLine returns ErrInterrupted.
 func ReadLine(prompt string) (line string, err error) {
 	tty, err := os.OpenFile("/dev/tty", os.O_RDWR, 0)
 	if err != nil {
@@ -150,18 +211,9 @@ func ReadLine(prompt string) (line string, err error) {
 	}
 	defer tty.Close()
 
-	tty.WriteString(prompt)
-	reader := &lineReader{
-		reader: bufio.NewReader(tty),
-		writer: bufio.NewWriter(tty),
-	}
-
-	err = WithRaw(tty.Fd(), func() error {
-		return reader.readLine()
-	})
-
+	reader := newLineReader(tty, prompt)
+	err = reader.readLine()
 	line = string(reader.buf)
 
-	tty.WriteString("\n")
 	return
 }
